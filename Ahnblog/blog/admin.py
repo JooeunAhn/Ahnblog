7b3fc33b04d341daf7e8042aa4ecb793e0db1676
JooeunAhn <jflow@hanyang.ac.kr>
2016-05-22 20:23:35 +0900

    Post 모델 등록 및 admin등록

--- Ahnblog/blog/admin.py
+++ Ahnblog/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from blog.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ['id', 'title', 'get_title_length']
+	list_display_links = ['title']
+
+	def get_title_length(self, post):
+		return len(post.title)
+
+admin.site.register(Post, PostAdmin)
 
 # Register your models here.
--- Ahnblog/blog/models.py
+++ Ahnblog/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+	title=models.CharField(max_length=100, help_text='제목은 100글자')
+	content=models.TextField()
+	photo=models.ImageField(blank=True)
+	create_at=models.DateTimeField(auto_now_add=True)
+	update_at=models.DateTimeField(auto_now=True)
